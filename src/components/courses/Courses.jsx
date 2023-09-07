4a59ba41911a1b5e3b7517c54948f44c45f661db
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-06 19:43:40 -0500

    Add courses section with basic heading.

---
+++ src/components/courses/Courses.jsx
@@ -0,0 +1,19 @@
+import styles from './courses.module.css'
+
+export default function Courses() {
+    return (
+        <section>
+
+            <div
+                className={styles.heading}
+            >
+
+                <h3>
+                    Check out our most popular courses!
+                </h3>
+
+            </div>
+
+        </section>
+    )
+}
